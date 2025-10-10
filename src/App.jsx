4eeb58c452bb7f880e956eb16ed444c5742fe68f
drb0r1s <drb0r1sdev@gmail.com>
2025-10-10 18:08:51 +0200

    Initialization of the project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+    return(
+        <h1>Assembly Simulator</h1>
+    );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./style/main.css";
+
+const root = document.getElementById("root");
+createRoot(root).render(<App />);
