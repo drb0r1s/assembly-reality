b7fe865cf811208bc8096dd179c92a8fea74cfc0
drb0r1s <drb0r1sdev@gmail.com>
2025-10-10 19:50:01 +0200

    Initialization of data/images, icons for header

---
+++ src/data/images.js
@@ -0,0 +1,11 @@
+// GENERAL
+import arrowDownIcon from "../images/arrow-down-icon.svg";
+import assembleIcon from "../images/assemble-icon.svg";
+import runIcon from "../images/run-icon.svg";
+import stepIcon from "../images/step-icon.svg";
+import resetIcon from "../images/reset-icon.svg";
+
+export const images = {
+    // GENERAL
+    arrowDownIcon, assembleIcon, runIcon, stepIcon, resetIcon
+};
