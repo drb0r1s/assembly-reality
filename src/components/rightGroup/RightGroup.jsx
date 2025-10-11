abac8d1033296e73ad68139a66cc29cf246fb042
drb0r1s <drb0r1sdev@gmail.com>
2025-10-11 17:17:38 +0200

    RightGroup component updates

--- src/components/rightGroup/RightGroup.jsx
+++ src/components/rightGroup/RightGroup.jsx
@@ -1,7 +1,13 @@
+import RightGroupDisplay from "./RightGroupDisplay";
+
 const RightGroup = () => {
     return(
         <div className="right-group">
-            
+            <div className="right-group-header">
+                <strong>Input / Output Devices</strong>
+            </div>
+
+            <RightGroupDisplay />
         </div>
     );
 }
