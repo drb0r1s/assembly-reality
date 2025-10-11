a0b113a254bf01ec445055b6304d45f18e80bc6f
drb0r1s <drb0r1sdev@gmail.com>
2025-10-11 17:43:16 +0200

    CPURegisters basic implementation

---
+++ src/components/rightGroup/CPURegisters.jsx
@@ -0,0 +1,14 @@
+import MiniHeader from "../MiniHeader";
+
+const CPURegisters = () => {
+    return(
+        <div className="cpu-registers">
+            <MiniHeader
+                title="CPU Registers"
+                style={{ cursor: "ns-resize" }}
+            />
+        </div>
+    );
+}
+
+export default CPURegisters;
