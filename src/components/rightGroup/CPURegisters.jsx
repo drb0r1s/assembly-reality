9f1e980d207b219db0f682b739eadf6b5a510414
drb0r1s <drb0r1sdev@gmail.com>
2025-10-11 19:10:46 +0200

    CPURegisters component update

--- src/components/rightGroup/CPURegisters.jsx
+++ src/components/rightGroup/CPURegisters.jsx
@@ -1,12 +1,73 @@
+import { useRef } from "react";
 import MiniHeader from "../MiniHeader";
 
 const CPURegisters = () => {
+    const registersRef = useRef({
+        A: "0000", B: "0000", C: "0000", D: "0000",
+        IP: "0000", SP: "0000",
+        SR: { M: "0", C: "0", Z: "0", F: "0", H: "0" }
+    });
+    
     return(
         <div className="cpu-registers">
             <MiniHeader
                 title="CPU Registers"
                 style={{ cursor: "ns-resize" }}
             />
+
+            <div className="cpu-registers-content">
+                <div className="cpu-registers-row">
+                    <div className="cpu-registers-row-register">
+                        <strong>A</strong>
+                        <p>{registersRef.current.A}</p>
+                    </div>
+
+                    <div className="cpu-registers-row-register">
+                        <strong>B</strong>
+                        <p>{registersRef.current.B}</p>
+                    </div>
+
+                    <div className="cpu-registers-row-register">
+                        <strong>C</strong>
+                        <p>{registersRef.current.C}</p>
+                    </div>
+
+                    <div className="cpu-registers-row-register">
+                        <strong>D</strong>
+                        <p>{registersRef.current.D}</p>
+                    </div>
+                </div>
+
+                <div className="cpu-registers-row">
+                    <div className="cpu-registers-row-register cpu-ip-register">
+                        <strong>IP</strong>
+                        <p>{registersRef.current.IP}</p>
+                    </div>
+
+                    <div className="cpu-registers-row-register cpu-sp-register">
+                        <strong>SP</strong>
+                        <p>{registersRef.current.SP}</p>
+                    </div>
+
+                    <div className="cpu-registers-row-register">
+                        <strong>SR</strong>
+
+                        <div className="cpu-registers-row-sr-values">
+                            {Object.keys(registersRef.current.SR).map((key, index) => {
+                                const value = Object.values(registersRef.current.SR)[index];
+
+                                return <div
+                                    key={index}
+                                    className="cpu-registers-row-sr-value"
+                                >
+                                    <p>{key}</p>
+                                    <p>{value}</p>
+                                </div>;
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
