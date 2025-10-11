5e343790dfe891611b230efca47f486558d74819
drb0r1s <drb0r1sdev@gmail.com>
2025-10-11 17:18:12 +0200

    Keyboard icons added to images.js

--- src/data/images.js
+++ src/data/images.js
@@ -4,8 +4,10 @@ import assembleIcon from "../images/assemble-icon.svg";
 import runIcon from "../images/run-icon.svg";
 import stepIcon from "../images/step-icon.svg";
 import resetIcon from "../images/reset-icon.svg";
+import keyboardIcon from "../images/keyboard-icon.svg";
+import keyboardBlueIcon from "../images/keyboard-blue-icon.svg";
 
 export const images = {
     // GENERAL
-    arrowDownIcon, assembleIcon, runIcon, stepIcon, resetIcon
+    arrowDownIcon, assembleIcon, runIcon, stepIcon, resetIcon, keyboardIcon, keyboardBlueIcon
 };
