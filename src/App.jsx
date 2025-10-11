47ff945d0804f472a23077e7ed8aca32aedfb82d
drb0r1s <drb0r1sdev@gmail.com>
2025-10-11 15:03:15 +0200

    App component update with display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Header from "./components/header/Header";
+import Editor from "./components/Editor";
+import Memory from "./components/Memory";
+import RightGroup from "./components/rightGroup/RightGroup";
 
 const App = () => {
     return(
         <main>
             <Header />
 
-            
+            <div className="display">
+                <Editor />
+                <Memory />
+                <RightGroup />
+            </div>
         </main>
     );
 }
