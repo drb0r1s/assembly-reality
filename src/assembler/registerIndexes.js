a5c706466d801765703d8f24212fc19473dd2c06
drb0r1s <drb0r1sdev@gmail.com>
2025-10-17 20:41:37 +0200

    registerIndexes

---
+++ src/assembler/registerIndexes.js
@@ -0,0 +1,17 @@
+export const registerIndexes = {
+    A: "00",
+    B: "01",
+    C: "02",
+    D: "03",
+    SP: "04",
+    IP: "05",
+    SR: "06",
+    AH: "07",
+    AL: "08",
+    BH: "09",
+    BL: "A",
+    CH: "B",
+    CL: "C",
+    DH: "D",
+    DL: "E"
+};
