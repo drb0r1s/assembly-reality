1596ddfc6358f546d963b01a615d018dd5e0f296
drb0r1s <drb0r1sdev@gmail.com>
2025-10-17 13:57:32 +0200

    Implementation of AST

---
+++ src/compiler/AST.js
@@ -0,0 +1,85 @@
+class ProgramNode {
+    constructor() {
+        this.type = "Program";
+        this.instructions = [];
+    }
+}
+
+class InstructionNode {
+    constructor(name, operands) {
+        this.type = "Instruction";
+        this.name = name;
+        this.operands = operands;
+    }
+}
+
+class OperandNode {
+    constructor(value) {
+        this.type = "Operand";
+        this.value = value;
+    }
+}
+
+class SeparatorNode {
+    constructor(value) {
+        this.type = "Separator";
+        this.value = value;
+    }
+}
+
+export const AST = {
+    build: tokens => {
+        const programNode = new ProgramNode();
+
+        // line.number contains the number of the line we're currently on
+        // line.tokens holds all the tokens from that specific line
+        const line = { number: tokens[0].line, tokens: [] };
+
+        for(let i = 0; i < tokens.length; i++) {
+            const token = tokens[i];
+
+            // New line is reached, we should parse all the tokens from the previous line first.
+            if(token.line !== line.number) {
+                if(line.tokens.length > 0) {
+                    const instructionNode = AST.parseInstruction(line.tokens);
+                    programNode.instructions.push(instructionNode);
+                }
+
+                // Reseting the line object.
+                line.number = token.line;
+                line.tokens = [];
+            }
+
+            line.tokens.push(token);
+        }
+
+        // When the loop finishes, parse anything that was left from the last line.
+        if(line.tokens.length > 0) {
+            const instructionNode = AST.parseInstruction(line.tokens);
+            programNode.instructions.push(instructionNode);
+        }
+
+        return programNode;
+    },
+
+    // tokens parameter contains all the tokens from the specific line
+    parseInstruction: tokens => {
+        const firstToken = tokens[0];
+
+        if(firstToken.type === "keyword") {
+            const instructionNode = new InstructionNode(firstToken.value, []);
+            
+            for(let i = 1; i < tokens.length; i++) {
+                if(tokens[i].type === "comment") continue; // AST ignores the comments.
+                instructionNode.operands.push(AST.parseOperand(tokens[i]));
+            }
+
+            return instructionNode;
+        }
+    },
+
+    parseOperand: token => {
+        if(token.value === ",") return new SeparatorNode(token.value);
+        return new OperandNode(token.value);
+    }
+};
