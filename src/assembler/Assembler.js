34dd373ada3d647787cac5b5c9022fbdc067fe53
drb0r1s <drb0r1sdev@gmail.com>
2025-10-17 20:42:00 +0200

    Assembler updates

--- src/assembler/Assembler.js
+++ src/assembler/Assembler.js
@@ -1,21 +1,80 @@
 import { Tokenizer } from "./Tokenizer";
 import { AST } from "./AST";
+import { instructionSet } from "./instructionSet";
+import { registerIndexes } from "./registerIndexes";
 
-export const Assembler = {
-    assemble: text => {
+class AssemblerError {
+    constructor(type, content) {
+        this.type = type;
+        this.content = content;
+    }
+}
+
+export class Assembler {
+    constructor() {
+        this.memory = {
+            matrix: Array.from({ length: 258 }, () => Array.from({ length: 16 }, () => "00")),
+            free: { i: 0, j: 0 } // Pointers to the last free-memory coordinates.
+        };
+    }
+    
+    assemble(text) {
         const tokens = Tokenizer.tokenize(text);
         const ast = AST.build(tokens);
-
-        const memoryMapMatrix = Array.from({ length: 258 }, () => Array.from({ length: 16 }, () => "00"));
+        console.log(ast)
 
         for(let i = 0; i < ast.instructions.length; i++) {
-            Assembler.assembleInstruction(ast.instructions[i]);
+            try {
+                this.assembleInstruction(ast.instructions[i]);
+            }
+
+            catch(error) {
+                if(error instanceof AssemblerError) return { error };
+            }
         }
 
-        return memoryMapMatrix;
-    },
+        return this.memory.matrix;
+    }
 
-    assembleInstruction: instruction => {
+    assembleInstruction(instruction) {
+        const instructionCode = instructionSet[instruction.name];
+        if(!instructionCode) throw new AssemblerError("UnknownInstruction", `${instruction.name} is an unknown instruction!`);
 
+        if(instruction.operands[1].type !== "Separator") throw new AssemblerError("MissingSeparator", `The separator is missing for the ${instruction.name} instruction!`);
+
+        const operands = instruction.operands.filter(operand => operand.type !== "Separator");
+
+        if(operands.length !== 2) throw new AssemblerError("InvalidOperands", `Instruction ${instruction.name} requires 2 operands!`);
+
+        const dest = operands[0];
+        const src = operands[1];
+
+        let destCode = parseInt(dest.value).toString(16).toUpperCase().padStart(4, "0");
+        let srcCode = parseInt(src.value).toString(16).toUpperCase().padStart(4, "0");
+
+        if(dest.valueType === "register") destCode = registerIndexes[dest.value];
+        if(src.valueType === "register") srcCode = registerIndexes[src.value];
+
+        this.memoryWrite(instructionCode + destCode + srcCode);
+    }
+
+    memoryWrite(hex) {
+        let { i, j } = this.memory.free;
+        const hexCells = hex.match(/.{1,2}/g); // Breaking hex string into groups of two digit numbers, to fit memory cells.
+
+        for(let k = 0; k < hexCells.length; k++) {
+            this.memory.matrix[i][j] = hexCells[k];
+
+            if(j === 16) {
+                i++;
+                j = 0;
+            }
+
+            else j++;
+
+            if(i > this.memory.matrix.length) throw new AssemblerError("OutOfMemory", "Memory limit exceeded!");
+        }
+
+        if(this.onMemoryChange) this.onMemoryChange(this.memory.matrix);
     }
 };
