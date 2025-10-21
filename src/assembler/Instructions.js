2f20bf2f9d6f3710896c27384f768573a0d09ecc
drb0r1s <drb0r1sdev@gmail.com>
2025-10-21 22:01:20 +0200

    Implementation of MOVB in Instructions

--- src/assembler/Instructions.js
+++ src/assembler/Instructions.js
@@ -5,12 +5,11 @@ import { registerIndexes } from "./registerIndexes";
 export const Instructions = {
     MOV: instruction => {
         const operands = instruction.operands.filter(operand => operand.type !== "Separator");
-        if(operands.length !== 2) throw new AssemblerError("InvalidOperands", `Instruction ${instruction.name} requires 2 operands!`);
+        if(operands.length !== 2) throw new AssemblerError("InvalidOperands", { name: instruction.name, operands: 2 });
 
         const instructionCode = InstructionSet[instruction.name](operands);
-        if(!instructionCode) throw new AssemblerError("UnknownInstruction", `${instruction.name} is an unknown instruction!`);
 
-        if(instruction.operands[1].type !== "Separator") throw new AssemblerError("MissingSeparator", `The separator is missing for the ${instruction.name} instruction!`);
+        if(instruction.operands[1].type !== "Separator") throw new AssemblerError("MissingSeparator", { name: instruction.name });
 
         const dest = operands[0];
         const src = operands[1];
@@ -25,8 +24,41 @@ export const Instructions = {
                 case "register": return registerIndexes[operand.value];
                 case "memory.register": return "00" + registerIndexes[operand.value];
                 case "memory.number.hex": return operand.value.toUpperCase().padStart(4, "0");
-                case "number.decimal": return parseInt(operand.value).toString(16).toUpperCase().padStart(4, "0");
+                case "number.decimal":
+                    if(operand.value > 65535) throw new AssemblerError("DecimalLimit16");
+                    return parseInt(operand.value).toString(16).toUpperCase().padStart(4, "0");
                 case "number.hex": return operand.value.toUpperCase().padStart(4, "0");
+                default: throw new AssemblerError("InvalidOperand", { operand: operand.value, instruction: instruction.name });
+            }
+        }
+    },
+
+    MOVB: instruction => {
+        const operands = instruction.operands.filter(operand => operand.type !== "Separator");
+        if(operands.length !== 2) throw new AssemblerError("InvalidOperands", { name: instruction.name, operands: 2 });
+
+        const instructionCode = InstructionSet[instruction.name](operands);
+
+        if(instruction.operands[1].type !== "Separator") throw new AssemblerError("MissingSeparator", { name: instruction.name });
+
+        const dest = operands[0];
+        const src = operands[1];
+
+        const destCode = parseType(dest);
+        const srcCode = parseType(src);
+
+        return instructionCode + destCode + srcCode;
+
+        function parseType(operand) {
+            switch(operand.valueType) {
+                case "half.register": return registerIndexes[operand.value];
+                case "memory.half.register": return registerIndexes[operand.value];
+                case "memory.number.hex": return operand.value.toUpperCase().padStart(2, "0");
+                case "number.decimal":
+                    if(operand.value > 255) throw new AssemblerError("DecimalLimit8");
+                    return parseInt(operand.value).toString(16).toUpperCase().padStart(2, "0");
+                case "number.hex": return operand.value.toUpperCase().padStart(2, "0");
+                default: throw new AssemblerError("InvalidOperand", { operand: operand.value, instruction: instruction.name });
             }
         }
     }
