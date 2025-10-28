f10eeab1523de052d11f7057730661f29571b5e3
drb0r1s <drb0r1sdev@gmail.com>
2025-10-28 19:26:15 +0100

    Implementation of Registers class

--- src/assembler/Assembler.js
+++ src/assembler/Assembler.js
@@ -1,18 +1,16 @@
 import { Tokenizer } from "./Tokenizer";
 import { AST } from "./AST";
 import { Instructions } from "./Instructions";
+import { Registers } from "./Registers";
 import { Memory } from "./Memory";
 import { Executor } from "./Executor";
+import { AssemblerError } from "./AssemblerError";
 
 export class Assembler {
     constructor() {
+        this.registers = new Registers();
         this.memory = new Memory();
         this.halted = false; // End the assembling of the code.
-        this.registers = {
-            A: "0000", B: "0000", C: "0000", D: "0000",
-            IP: "0000", SP: "0000",
-            SR: { M: "0", C: "0", Z: "0", F: "0", H: "0" }
-        };
     }
     
     assemble(text) {
@@ -20,7 +18,7 @@ export class Assembler {
         const ast = AST.build(tokens);
         console.log(ast)
 
-        if(this.memory.free.i !== 0 || this.memory.free.j !== 0) this.memory.reset(); // We need to clean the memory before assembling the code.
+        this.reset(); // We need to reset the assembler before reassembling the code.
 
         for(let i = 0; i < ast.instructions.length; i++) {
             try {
@@ -108,41 +106,8 @@ export class Assembler {
     }
 
     reset() {
+        this.registers.reset();
         this.memory.reset();
         this.halted = false;
-
-        this.registers = {
-            A: "0000", B: "0000", C: "0000", D: "0000",
-            IP: "0000", SP: "0000",
-            SR: { M: "0", C: "0", Z: "0", F: "0", H: "0" }
-        };
-    }
-};
-
-export class AssemblerError {
-    constructor(type, attributes, line) {
-        this.type = type;
-        this.content = this.getContent(attributes);
-        this.line = line ? line : null;
-    }
-
-    getContent(attributes) {
-        switch(this.type) {
-            case "UnknownInstruction": return `${attributes.name} is an unknown instruction!`;
-            case "UnknownInstructionCode": return `${attributes.code} doesn't represent any instruction!`;
-            case "UnknownExecutableType": return `"${attributes.type}" is not valid for executable ${attributes.instruction}!`;
-            case "InvalidOperandsCombination": return `Combination of ${attributes.operands[0]} and ${attributes.operands[1]} operands is invalid for the ${attributes.instruction} instruction!`;
-            case "InvalidOperand": return `Operand ${attributes.operand} is invalid for the ${attributes.instruction} instruction!`;
-            case "InvalidNumberOfOperands": return `Instruction ${attributes.name} requires ${attributes.operands} operand${attributes.operands !== 1 ? "s" : ""}!`;
-            case "InvalidNumberOfArguments": return `Instruction ${attributes.instruction} requires ${attributes.required} argument${attributes.required !== 1 ? "s" : ""}, but received ${attributes.received}!`;
-            case "MissingSeparator": return `The separator is missing for the ${attributes.name} instruction!`;
-            case "DecimalLimit16": return "16-bit operand must have a value between 0 and 65535!";
-            case "DecimalLimit8": return "8-bit operand must have a value between 0 and 255!";
-            case "DecimalMemoryLimit": return "Decimal memory pointer operand must have a value between 0 and 4127!";
-            case "HexLimit16": return "16-bit operand must have a value between 0x0000 and 0xFFFF!";
-            case "HexLimit8": return "8-bit operand must have a value between 0x00 and 0xFF!";
-            case "HexMemoryLimit": return "Hex memory pointer operand must have a value between 0x0000 and 0x101F!";
-            case "OutOfMemory": return "Memory limit exceeded!";
-        }
     }
 };
--- src/assembler/Registers.js
+++ src/assembler/Registers.js
@@ -1,3 +1,5 @@
+import { Manager } from "../Manager";
+
 const registerIndex = {
     A: "00",
     B: "01",
@@ -34,12 +36,74 @@ const indexRegister = {
     "0E": "DL"
 };
 
-export const Registers = {
-    getIndex: register => {
-        return registerIndex[register];
-    },
+export class Registers {
+    constructor() {
+        this.A = "0000";
+        this.B = "0000";
+        this.C = "0000";
+        this.D = "0000";
 
-    getRegister: index => {
+        this.IP = "0000";
+        this.SP = "0000";
+
+        this.SR = { M: "0", C: "0", Z: "0", F: "0", H: "0" };
+    }
+
+    get(index) {
         return indexRegister[index];
+    }
+
+    getValue(register) {
+        if(register.includes("H") || register.includes("L")) {
+            const isH = register.includes("H");
+
+            register = register[0];
+            const registerValue = this.getValue(register);
+
+            const cell = isH ? registerValue.slice(0, 2) : registerValue.slice(-2);
+
+            // Even though we expect 8-bit value, it is important to still return 16-bit value, with second cell empty.
+            // This way we can "synchronize" this operation with others (e.g. memory.number.* type will call memory.point() which will result in 16-bit return value).
+            return `${cell}00`;
+        }
+
+        return this[register];
+    }
+
+    getIndex(register) {
+        return registerIndex[register];
+    }
+
+    getValueByIndex(index) {
+        const register = this.get(index);
+        return this.getValue(register);
+    }
+
+    update(register, value) {
+        if(register.includes("H") || register.includes("L")) {
+            const isH = register.includes("H");
+            
+            register = register[0];
+
+            const updatedCell = value.slice(0, 2);
+            const oldCell = isH ? this.getValue(register).slice(-2) : this.getValue(register).slice(0, 2);
+
+            value = isH ? `${updatedCell}${oldCell}` : `${oldCell}${updatedCell}`;
+        }
+
+        this[register] = value;
+        Manager.trigger("registerUpdate", this);
+    }
+
+    reset() {
+        this.A = "0000";
+        this.B = "0000";
+        this.C = "0000";
+        this.D = "0000";
+
+        this.IP = "0000";
+        this.SP = "0000";
+
+        this.SR = { M: "0", C: "0", Z: "0", F: "0", H: "0" };
     }
 };
