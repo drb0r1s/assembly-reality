2eee614652d93d6319a05be1a7b5c30adf27a1d7
drb0r1s <drb0r1sdev@gmail.com>
2025-12-08 20:43:37 +0100

    Implementation of Stack object

---
+++ src/assembler/Stack.js
@@ -0,0 +1,26 @@
+import { AssemblerError } from "./AssemblerError";
+
+export const Stack = {
+    push: (assembler, value, options) => {
+        const isHalf = options?.isHalf ? options.isHalf : false;
+        
+        assembler.memory.rewrite(assembler.cpuRegisters.getValue("SP"), value, { isHalf, isStack: true });
+
+        const numberOfCells = isHalf ? 1 : 2;
+        assembler.cpuRegisters.update("SP", assembler.cpuRegisters.getValue("SP") - numberOfCells);
+    },
+
+    pop: (assembler, popRegister, options) => {
+        const isHalf = options?.isHalf ? options.isHalf : false;
+        const numberOfCells = isHalf ? 1 : 2;
+        
+        if(assembler.cpuRegisters.getValue("SP") + numberOfCells > assembler.memory.stackStart) throw new AssemblerError("StackUnderflow");
+        
+        assembler.cpuRegisters.update("SP", assembler.cpuRegisters.getValue("SP") + numberOfCells);
+        
+        const popped = assembler.memory.point(assembler.cpuRegisters.getValue("SP"), { isHalf, isStack: true });
+        if(popRegister) assembler.cpuRegisters.update(popRegister, popped);
+
+        return popped;
+    }
+};
