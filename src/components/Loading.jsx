e5fc4185887b5e2820aedeb1caac171a29a76c17
drb0r1s <drb0r1sdev@gmail.com>
2026-02-03 16:23:59 +0100

    Loading component created

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+    return(
+        <div className="loading">
+            <div className="loading-outer-circle">
+                <div className="loading-inner-circle"></div>
+                <div className="loading-spinner"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Loading;
