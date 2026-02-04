dda276ad8957b33e9ff0b43230b7dee7d9eca8f3
drb0r1s <drb0r1sdev@gmail.com>
2026-02-04 22:35:41 +0100

    Generalization of useLinkedResizing

--- src/hooks/useLinkedResizing.js
+++ src/hooks/useLinkedResizing.js
@@ -1,36 +1,32 @@
 import { useEffect } from "react";
+import { useResize } from "./useResize";
 import { useManagerValue } from "./useManagerValue";
 
-export const useLinkedResizing = ({ headerRef, elementRef, holderRef, collisionRefs }) => {    
+export const useLinkedResizing = ({ headerRef, elementRefs, targetIndex, holderRef, conditional }) => {    
     const headerHeight = 27;
 
+    const width = useResize();
     const view = useManagerValue("view");
     
     useEffect(() => {
+        if(width >= 1300 && conditional) return;
+
         const y = { start: 0, last: 0 };
         const heights = { start: 0, holder: 0 };
 
-        const tops = {
-            element: null,
-            prev: null,
-            doublePrev: null,
-            next: null,
-            doubleNext: null
-        };
-
+        let elementTops = [];
         let lockElement = [];
 
         const handleMouseDown = e => {
             y.start = e.clientY;
 
-            heights.start = elementRef.current.offsetHeight;
+            heights.start = elementRefs[targetIndex].current.offsetHeight;
             heights.holder = holderRef.current.offsetHeight;
 
-            tops.element = elementRef.current.offsetTop;
-            tops.prev = collisionRefs?.prev?.current?.offsetTop;
-            tops.doublePrev = collisionRefs?.doublePrev?.current?.offsetTop;
-            tops.next = collisionRefs?.next?.current?.offsetTop;
-            tops.doubleNext = collisionRefs?.doubleNext?.current?.offsetTop;
+            elementTops = [];
+            for(let i = 0; i < elementRefs.length; i++) elementTops.push(elementRefs[i].current.offsetTop);
+
+            lockElement = [];
 
             document.body.style.userSelect = "none";
             
@@ -43,75 +39,50 @@ export const useLinkedResizing = ({ headerRef, elementRef, holderRef, collisionR
             const direction = e.clientY > y.last ? "down" : "up";
 
             y.last = e.clientY;
-            
-            if(collisionRefs.prev) {
-                if(collisionRefs.doublePrev) {
-                    if(lockElement.includes("doublePrev") && direction === "down") unlock("doublePrev");
 
-                    else if(lockElement.includes("doublePrev") || (tops.prev < tops.doublePrev + headerHeight)) {
-                        let newHeight = heights.start + deltaY + 2 * headerHeight;
-                        lockElement.push("doublePrev");
+            for(let i = 0; i <= targetIndex; i++) {
+                if(lockElement.includes(i) && direction === "down") unlock(i);
 
-                        if(newHeight > heights.holder) newHeight = heights.holder;
+                else if(
+                    lockElement.includes(i) ||
+                    (elementTops[i + 1] < elementTops[i] + headerHeight)
+                ) {    
+                    let newHeight = heights.start + deltaY + (targetIndex - i) * headerHeight;
+                    if(i !== targetIndex) lockElement.push(i);
 
-                        collisionRefs.doublePrev.current.style.height = `${newHeight}px`;
-                        tops.doublePrev = collisionRefs.doublePrev.current.offsetTop;
-                    }
-                }
-                
-                if(lockElement.includes("prev") && direction === "down") unlock("prev");
-                
-                else if(lockElement.includes("prev") || (tops.element < tops.prev + headerHeight)) {
-                    let newHeight = heights.start + deltaY + headerHeight;
-                    lockElement.push("prev");
-
-                    const multiply = collisionRefs?.doublePrev?.current ? 1 : 0;
+                    const multiply = i;
                     if(newHeight > heights.holder - multiply * headerHeight) newHeight = heights.holder - multiply * headerHeight;
-
-                    collisionRefs.prev.current.style.height = `${newHeight}px`;
-                    tops.prev = collisionRefs.prev.current.offsetTop;
+                    
+                    elementRefs[i].current.style.height = `${newHeight}px`;
+                    elementTops[i] = elementRefs[i].current.offsetTop;
                 }
             }
 
-            if(collisionRefs.next) {
-                if(collisionRefs.doubleNext) {
-                    if(lockElement.includes("doubleNext") && direction === "up") unlock("doubleNext");
+            for(let i = elementRefs.length - 1; i >= targetIndex; i--) {
+                if(lockElement.includes(i) && direction === "up") unlock(i);
 
-                    else if(lockElement.includes("doubleNext") || (tops.next + headerHeight > tops.doubleNext)) {
-                        let newHeight = heights.start + deltaY - 2 * headerHeight;
-                        lockElement.push("doubleNext");
+                else if(
+                    lockElement.includes(i) ||
+                    (elementTops[i - 1] + headerHeight > elementTops[i])
+                ) {
+                    let newHeight = heights.start + deltaY - (i - targetIndex) * headerHeight;
+                    if(i !== targetIndex) lockElement.push(i);
 
-                        if(newHeight < headerHeight) newHeight = headerHeight;
-
-                        collisionRefs.doubleNext.current.style.height = `${newHeight}px`;
-                        tops.doubleNext = collisionRefs.doubleNext.current.offsetTop;
-                    }
-                }
-                
-                if(lockElement.includes("next") && direction === "up") unlock("next");
-
-                else if(lockElement.includes("next") || (tops.element + headerHeight > tops.next)) {
-                    let newHeight = heights.start + deltaY - headerHeight;
-                    lockElement.push("next");
-
-                    const multiply = collisionRefs?.doubleNext?.current ? 2 : 1;
+                    const multiply = elementRefs.length - i;
                     if(newHeight < multiply * headerHeight) newHeight = multiply * headerHeight;
 
-                    collisionRefs.next.current.style.height = `${newHeight}px`;
-                    tops.next = collisionRefs.next.current.offsetTop;
+                    elementRefs[i].current.style.height = `${newHeight}px`;
+                    elementTops[i] = elementRefs[i].current.offsetTop;
                 }
             }
 
             let newHeight = heights.start + deltaY;
 
-            const multiplyPrev = getMultiply("prev");
-            const multiplyNext = getMultiply("next");
+            if(newHeight > heights.holder - targetIndex * headerHeight) newHeight = heights.holder - targetIndex * headerHeight;
+            if(newHeight < (elementRefs.length - targetIndex) * headerHeight) newHeight = (elementRefs.length - targetIndex) * headerHeight;
 
-            if(newHeight > heights.holder - multiplyPrev * headerHeight) newHeight = heights.holder - multiplyPrev * headerHeight;
-            if(newHeight < multiplyNext * headerHeight) newHeight = multiplyNext * headerHeight;
-
-            elementRef.current.style.height = `${newHeight}px`;
-            tops.element = elementRef.current.offsetTop;
+            elementRefs[targetIndex].current.style.height = `${newHeight}px`;
+            elementTops[targetIndex] = elementRefs[targetIndex].current.offsetTop;
 
             function unlock(type) {
                 const newLockElement = [];
@@ -121,26 +92,6 @@ export const useLinkedResizing = ({ headerRef, elementRef, holderRef, collisionR
                 }
 
                 lockElement = newLockElement;
-            }
-
-            function getMultiply(type) {
-                if(type === "prev") {
-                    if(!collisionRefs?.prev?.current) return 0;
-                    
-                    else {
-                        if(!collisionRefs?.doublePrev?.current) return 1;
-                        return 2;
-                    }
-                }
-
-                else {
-                    if(!collisionRefs?.next?.current) return 1;
-
-                    else {
-                        if(!collisionRefs?.doubleNext?.current) return 2;
-                        return 3;
-                    }
-                }
             }
         }
 
@@ -153,5 +104,5 @@ export const useLinkedResizing = ({ headerRef, elementRef, holderRef, collisionR
         
         headerRef.current.addEventListener("mousedown", handleMouseDown);
         return () => { if(headerRef.current) headerRef.current.removeEventListener("mousedown", handleMouseDown) }
-    }, [headerRef, view]);
+    }, [view, headerRef, elementRefs]);
 }
