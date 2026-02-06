612e2c4052ecb53703cb67d8b26839659c13df9a
drb0r1s <drb0r1sdev@gmail.com>
2026-02-06 19:33:01 +0100

    EditorError component update for mobile layout

--- src/components/editor/EditorError.jsx
+++ src/components/editor/EditorError.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useRef } from "react";
+import { useResize } from "../../hooks/useResize";
 import { Images } from "../../data/Images";
 
 const EditorError = ({ error, setError }) => {
     const editorErrorRef = useRef(null);
+    const width = useResize();
 
     useEffect(() => {
         setTimeout(() => {
             editorErrorRef.current.style.opacity = "1";
-            editorErrorRef.current.style.bottom = "0";
+            editorErrorRef.current.style.bottom = (width < 900 ? "48px" : "0"); // 48px = MobileNavigation height
         }, 100);
     }, []);
 
