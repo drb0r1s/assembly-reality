1540b7ae5efad54c7e7226b4c84f5420f2c21380
drb0r1s <drb0r1sdev@gmail.com>
2026-02-10 23:15:14 +0100

    Executable out method updates

--- src/assembler/backend/Executable.js
+++ src/assembler/backend/Executable.js
@@ -379,7 +379,7 @@ export class Executable {
                         // TEXT (1) or BITMAP (2)
                         case 1:
                         case 2:
-                            if(prevVidMode > 0 && prevVidMode === registerValue) return;
+                            if(prevVidMode > 0) return;
                             self.postMessage({ action: "graphicsEnabled", data: registerValue });
                             break;
                         // CLEAR
