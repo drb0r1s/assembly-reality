ebb182b8f5e7b967c63a6056c6bc49f2fc8cb530
drb0r1s <drb0r1sdev@gmail.com>
2026-03-21 23:13:44 +0100

    noHeaders control added to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import DesktopLayout from "./layouts/DesktopLayout";
 import MobileLayout from "./layouts/MobileLayout";
+import NoHeadersLayout from "./layouts/NoHeadersLayout";
 import { useResize } from "./hooks/useResize";
 import { useManagerValue } from "./hooks/useManagerValue";
 import { Autosave } from "./helpers/Autosave";
 
 const App = () => {
+    const [noHeaders, setNoHeaders] = useState(false);
+    
     const width = useResize();
     const isLightTheme = useManagerValue("isLightTheme");
 
-    useEffect(Autosave.initialize, []);
+    useEffect(() => {
+        // No COOP/COEP headers.
+        if(!crossOriginIsolated) setNoHeaders(true);
+
+        Autosave.initialize();
+    }, []);
 
     useEffect(() => {
         document.documentElement.setAttribute("data-theme", isLightTheme ? "light" : "dark");
     }, [isLightTheme]);
 
     return(
-        <>
-            {width >= 900 ? <DesktopLayout /> : <MobileLayout />}
-        </>
+        <div id="assembly-reality">
+            {noHeaders ? <NoHeadersLayout /> : width >= 900 ? <DesktopLayout /> : <MobileLayout />}
+        </div>
     );
 }
 
