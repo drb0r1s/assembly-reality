98928e33f3b485347d172bd31c5107c294673bf0
drb0r1s <drb0r1sdev@gmail.com>
2026-04-18 23:24:41 +0200

    Icons update in ThemeDashboard

--- src/components/themeDashboard/ThemeDashboard.jsx
+++ src/components/themeDashboard/ThemeDashboard.jsx
@@ -1,9 +1,22 @@
 import ThemeDashboardDropdown from "./ThemeDashboardDropdown";
+import { useManagerValue } from "../../hooks/useManagerValue";
 import { useDropdown } from "../../hooks/useDropdown";
 import { Images } from "../../data/Images";
 
 const ThemeDashboard = () => {
+    const theme = useManagerValue("theme");
     const { dropdown, enableDropdown, disableDropdown, dropdownRefs } = useDropdown({ theme: false });
+
+    const ThemeIcons = {
+        dark: Images.DarkThemeIcon,
+        light: Images.LightThemeIcon,
+        classic: Images.ClassicThemeIcon,
+        ocean: Images.OceanThemeIcon,
+        forest: Images.ForestThemeIcon,
+        ruby: Images.RubyThemeIcon
+    };
+
+    const ThemeIcon = ThemeIcons[theme];
     
     return(
         <div
@@ -17,8 +30,8 @@ const ThemeDashboard = () => {
             
             <div className="theme-dashboard-button">
                 <div className="theme-dashboard-button-left-group">
-                    <Images.ThemeIcon />
-                    <p>Select Theme</p>
+                    <ThemeIcon />
+                    <p>{theme} Theme</p>
                 </div>
             
                 <Images.LeftArrowIcon className="theme-dashboard-button-arrow" />
